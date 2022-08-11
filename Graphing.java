0d5c69e418ad2542cee4d3ee408a86cee3217ac6
Krishay Bhople <krishay.db23@gmail.com>
2022-08-10 22:24:11 -0700

    Optimized code slightly ~ Screen size and position can be set with variables now ~ Black background ~ Does not draw a line from the last point to the first point

--- Graphing.java
+++ Graphing.java
@@ -9,9 +9,12 @@ public class Graphing {
         GraphingPanel graphingPan = new GraphingPanel();
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 
-        frame.setSize(new Dimension(300, 400));
-        frame.setLocation((int)(screenSize.getWidth()/2 - 150), (int)(screenSize.getHeight()/2 - 200));
-        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+        int scrnWidth = 310;
+        int scrnHeight = 400;
+
+        frame.setSize(new Dimension(scrnWidth, scrnHeight));
+        frame.setLocation((int)(screenSize.getWidth()/2 - scrnWidth/2), (int)(screenSize.getHeight()/2 - scrnHeight/2));
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.getContentPane().add(graphingPan);
         frame.setVisible(true);
     }
@@ -34,6 +37,7 @@ class GraphingPanel extends JPanel {
         readFileAndInitArrays();
 
         setLayout(new BorderLayout());
+        setBackground(Color.BLACK);
     }
 
     public void readFileAndInitArrays() {
@@ -72,11 +76,14 @@ class GraphingPanel extends JPanel {
 
     @Override
     public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.WHITE);
         g.drawLine(100, 100, 100, 200);
         g.drawLine(100, 200, 200, 200);
 
         for (int i = 1; i < xVals.size(); i++) {
-            g.drawLine(previousPoint[0] + 100, 200 - previousPoint[1], xVals.get(i) + 100, 200 - yVals.get(i));
+            if (previousPoint[0] != xVals.get(xVals.size() - 1) && previousPoint[1] != yVals.get(yVals.size() - 1))
+                g.drawLine(previousPoint[0] + 100, 200 - previousPoint[1], xVals.get(i) + 100, 200 - yVals.get(i));
 
             previousPoint[0] = xVals.get(i);
             previousPoint[1] = yVals.get(i);
